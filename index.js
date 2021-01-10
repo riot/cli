1f2919c204ddc8c24a8ac53b611ec09ca206e344
gianlucaguarini <gianluca.guarini@gmail.com>
2021-01-10 15:59:08 +0100

    updated: generate cli file

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-#!/usr/bin/env node
-
-require = require('esm')(module) // eslint-disable-line
-const cli = require('./src/index.js')
+import run, * as cli from './src/index'
 
 // Run from CLI or as Node module
 if (module.parent) {
   module.exports = cli
-/* istanbul ignore next */
-} else cli.default(process.argv)
+  /* istanbul ignore next */
+} else run(process.argv)
---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import json from '@rollup/plugin-json'
+
+export default {
+  input: 'index.js',
+  output: {
+    banner: '#!/usr/bin/env node',
+    file: 'cli.js',
+    format: 'cjs'
+  },
+  plugins: [json()]
+}
