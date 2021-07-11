ed4d12d04de6d65e4f9d8a626a4422d7e34b07f1
gianlucaguarini <gianluca.guarini@gmail.com>
2021-07-11 17:52:45 +0200

    fixes #50

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,18 @@
+import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
+import {nodeResolve} from '@rollup/plugin-node-resolve'
 
 export default {
   input: 'index.js',
+  external: ['rollup'],
   output: {
     banner: '#!/usr/bin/env node',
     file: 'cli.js',
     format: 'cjs'
   },
-  plugins: [json()]
+  plugins: [
+    json(),
+    nodeResolve(),
+    commonjs()
+  ]
 }
