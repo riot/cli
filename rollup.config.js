d8975d7b828c47a5934171a224b04c8408aedccb
gianlucaguarini <gianluca.guarini@gmail.com>
2021-07-14 20:08:59 +0200

    Revert "fixes #50"
    This reverts commit ed4d12d04de6d65e4f9d8a626a4422d7e34b07f1.

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,11 @@
-import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
-import {nodeResolve} from '@rollup/plugin-node-resolve'
 
 export default {
   input: 'index.js',
-  external: ['rollup'],
   output: {
     banner: '#!/usr/bin/env node',
     file: 'cli.js',
     format: 'cjs'
   },
-  plugins: [
-    json(),
-    nodeResolve(),
-    commonjs()
-  ]
+  plugins: [json()]
 }
