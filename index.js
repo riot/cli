eddb3e98b84e1e3d3c9a474208fc621bee98477e
gianlucaguarini <gianluca.guarini@gmail.com>
2023-08-01 01:04:56 +0200

    fixed: Require main detection

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import process from 'node:process'
 import run, * as cli from './src/index.js'
+import { basename } from 'node:path'
+import { pathToFileURL } from 'node:url'
 
 export default cli
 
 // Auto-Run the CLI if this file is not imported
-if (process.mainModule) {
+if (basename(pathToFileURL(process.argv[1]).href) === 'riot') {
   /* c8 ignore next */
   run(process.argv)
 }
