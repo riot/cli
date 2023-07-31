6b1d8e12fbe0b3b2acafcf765cdabc23661fff2a
gianlucaguarini <gianluca.guarini@gmail.com>
2023-08-01 00:34:20 +0200

    fix: build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,15 @@
-import json from '@rollup/plugin-json'
-
 export default {
   input: 'index.js',
-  output: {
-    banner: '#!/usr/bin/env node',
-    file: 'cli.js',
-    format: ['es', 'cjs'],
-  },
-  plugins: [json()],
+  output: [
+    {
+      banner: '#!/usr/bin/env node',
+      file: 'cli.js',
+      format: 'es',
+    },
+    {
+      banner: '#!/usr/bin/env node',
+      file: 'cli.cjs',
+      format: 'cjs',
+    },
+  ],
 }
--- src/util.js
+++ src/util.js
@@ -1,10 +1,12 @@
 import { babel } from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import riot from 'rollup-plugin-riot'
 
 export function getRollupPlugins(options) {
   return [
+    json(options.json),
     riot(options.riot),
     nodeResolve(options.resolve),
     commonjs(options.commonjs),
